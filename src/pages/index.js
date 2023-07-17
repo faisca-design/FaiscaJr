9b8e19d99409d5db6d92934e2c9467d7e3d5035b
Unfollow1 <109428575+Unfollow1@users.noreply.github.com>
2023-07-17 01:06:50 -0300

    passando pro git issue#5

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,17 @@
-import Link from 'next/link'
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import { handleJSONfiles } from '@/utils/functions/jsonHandler'
+import styles from "./../styles/LearnMore.module.css";
+import GenericButton from "@/components/components/GenericButton";
 
-
-export default function Home({exemplos}) {
-  console.log(exemplos)
+function App() {
   return (
-    <div className={styles['container']}>
-      <Head><title>Template Next Netlify</title></Head>
-      <h1 className={styles['header']}>Este é um template de Next - Netlify</h1>
-      <p className={styles['subtitle']}>Abra /admin para ver as opções</p>
-      <ul className={styles['exemplo-list']}>
-        {exemplos && exemplos.map((exemplo,i) => (
-          <Link className={styles["link"]} key={i} href={`/exemplos/${exemplo.fileName}`}>
-            <div className={styles["card"]}>
-                <span >{exemplo.titulo}</span>
-            </div>
-          </Link>
-        ))}
-      </ul>
+    <div className={styles.Container}>
+      <h1 className={styles.textAbout}>
+        nós fazemos <span className={styles.altText}>desing</span> <br></br>de um jeito que você <br></br> nunca viu.
+
+      </h1>
+      <GenericButton ButtonText="Saiba mais sobre nós" />
+
     </div>
-  )
+  );
 }
 
-export async function getStaticProps(){
-  const exemplos = handleJSONfiles("./content/exemplos");
-  return {
-    props: { exemplos },
-  };
-}
+export default App;
