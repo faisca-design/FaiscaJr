bf24d890917c815886911062a8364e9aa615abdb
ArthurTuy <arthursauer10@outlook.com>
2023-07-17 21:51:07 -0300

    Issue #17,#18
    Issue 17 e 18 com uma parte do carrosel, nao finalizado

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,5 +1,37 @@
-function Portfolio(){
-    return <h1>Portfólio</h1>
+function Portfolio() {
+    return (
+    <html>
+        <head>
+            <link rel="stylesheet" href="../layout/Portfolio.css" />
+            <link rel="stylesheet" href="owlCarousel/dist/assets/owl.carousel.min.css" />
+            <link rel="stylesheet" href="owlCarousel/dist/assets/owl.theme.default.min.css" />
+        </head>
+        <body>
+            <script src=""></script>
+            <script src="owlCarousel/dist/owl.carousel.min.js"></script>
+            <main class="container">
+                <div class="home">
+                </div>
+                <div class="projeto">
+                    <h1 class="projetoTitle">
+                        Promover transformação social através <br>da <span>força</span> das raízes brasileiras,
+                        realçando <br>sua <span>energia moderna</span> e <span>vibrante</span>.
+                        <img class="img-carimbo" src="../components/img/CarimboPreto.png">
+                    </h1>
+                    </div>
+                    <div class="slider">
+                    <h2 class="depoimentosTitle">O QUE NOSSO CLIENTES TEM A DIZER</h2>
+                    <h2 class="depoimentosText">
+                        “Trabalhar com a faísca foi uma excelente experiência, o resultado superou
+                        toda e qualquer expectativa. Nos impactou de tal forma que buscamos estabelecer
+                        uma parceria e recomendamos o trabalho sempre que possível”
+                        <br><b>Equipe IECBiot</b>
+                    </h2>
+                    </div>
+            </main>
+        </body>
+    </html>
+    )
 }
 
 export default Portfolio
