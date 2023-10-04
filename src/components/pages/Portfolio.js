1bcaf6edf5b6178970e48a07ba74b13f63a127b5
Carol <102159304+Carol1207@users.noreply.github.com>
2023-10-04 16:31:33 -0300

    Banners
    Implementação de fotos para o banner, com o texto centralizado horizontalmente entre elas e uma pequena margem de divisória. Ainda há problema com a centralização vertical do texto - o primeiro banner possui o texto mais à esquerda que o restante.

---
+++ src/components/pages/Portfolio.js
@@ -0,0 +1,12 @@
+import BannersFaisca from "./Banners";
+
+function Portfolio() {
+    return (
+      <div>
+        <h1><br /></h1>
+        {BannersFaisca()}
+      </div>
+    );
+  }
+
+export default Portfolio
