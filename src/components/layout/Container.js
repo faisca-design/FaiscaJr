8a675c28f985e1928facc1edf89e6481ce7a2b60
Carol <carolinapfal2004@gmail.com>
2023-10-04 14:20:15 -0300

    issue#3#9
    Commit atualizado com os ícones originais da Faísca

--- src/components/layout/Container.js
+++ src/components/layout/Container.js
@@ -1,5 +1,3 @@
-//import styles from './Container.module.css'
-
 function Container(props){
     return (
         <div className={'${styles.container} ${styles[props.customClass]}'}>
--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,33 +1,36 @@
-import {IoMail, FaInstagram, FaLinkedin, FaBehance} from 'react-icons/fa'
-import styles from './Footer.module.css'
+import styles from './Footer.module.css';
+import emailIcon from './footerIcons/emailIcon.png';
+import instagramIcon from './footerIcons/instagramIcon.png';
+import linkedinIcon from './footerIcons/linkedinIcon.png';
+import behanceIcon from './footerIcons/behanceIcon.png';
 
 function Footer(){
     return (
         <footer className={styles.footer}>
             <ul className={styles.social_list}>
                 <li>
-                    <a href="https://github.com/seu-usuario" target="_blank" rel="noopener noreferrer">
-                        <IoMail style={{ color: '#d45827' }}/>
+                    <a href="https://mail.google.com/" target="_blank" rel="noopener noreferrer">
+                        <img src={emailIcon} alt="emailIcon" style={{ width: '24px', height: '24px', color: '#d45827' }} />
                     </a>
                 </li>
                 <li>
                     <a href="https://www.instagram.com/faiscadesignjr/" target="_blank" rel="noopener noreferrer">
-                        <FaInstagram style={{ color: '#d45827' }}/>
+                        <img src={instagramIcon} alt="instagramIcon" style={{ width: '24px', height: '24px', color: '#d45827' }} />
                     </a>
                 </li>
                 <li>
                     <a href="https://br.linkedin.com/company/faiscadesignjunior" target="_blank" rel="noopener noreferrer">
-                        <FaLinkedin style={{ color: '#d45827' }}/>
+                        <img src={linkedinIcon} alt="linkedinIcon" style={{ width: '24px', height: '24px', color: '#d45827' }} /> 
                     </a>
                 </li>
                 <li>
                     <a href="https://www.behance.net/FaiscaDesignJunior" target="_blank" rel="noopener noreferrer">
-                        <FaBehance style={{ color: '#d45827' }}/>
+                        <img src={behanceIcon} alt="behanceIcon" style={{ width: '24px', height: '24px', color: '#d45827' }} />
                     </a>
                 </li>
             </ul>
         </footer>
-    )
+    );
 }
 
 export default Footer
