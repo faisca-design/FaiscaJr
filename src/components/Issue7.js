31ef95b457e89225833aa43ef096944185dbfd01
Gabriel Arusiewicz Berta <gabriaru@gmail.com>
2023-10-12 02:48:15 -0300

    Imagens e textos editavei, ajuste nos espaçamentos das imagens #7

--- src/components/Issue7.js
+++ src/components/Issue7.js
@@ -1,7 +1,7 @@
 import styles from '../styles/Issue7.module.css'
-import Imagem from 'next/image'
+import Image from 'next/image'
 
-function Issue7 ()
+function Issue7 ({titulo1, titulo2, imagem, imagem2, imagem3, imagem4, imagem5, nomeprojeto1, nomeprojeto2, nomeprojeto3, nomeprojeto4, nomeprojeto5})
 {
     return(
         <div className={styles.container}>
@@ -14,45 +14,45 @@ function Issue7 ()
               </h1>
             </div>
             <h1 className={styles.subTitle}>
-                CASES
+                {titulo1}
                <hr className={styles.bar} width={500} />
-                SELECIONADOS
+                {titulo2}
             </h1>
             <div className={styles.containerImages}>
                 <div className={styles.imageRow}>
                     <figure className={styles.figure}>
+                        <Image className={styles.image}  src={imagem} width={440} height={530} alt="imagem temporaria de testes"/>
                         <label className={styles.text}>
-                            Nome do Projeto
+                            {nomeprojeto1}
                         </label>
-                        <img className={styles.image}  src="/img/RetanguloIlustrativo.png" width={440} height={530} alt="imagem temporaria de testes"/>
                     </figure>
                     <figure className={styles.figure}>
-                        <label className={styles.text}>
-                            Nome do Projeto
+                         <Image className={styles.image2}  src={imagem2} width={440} height={775} alt="imagem temporaria de testes"/>
+                        <label className={styles.text2}>
+                            {nomeprojeto2}
                         </label>
-                        <img className={styles.image} src="/img/RetanguloIlustrativo.png" width={440} height={775} alt="imagem temporaria de testes"/>
                     </figure>
                 </div>
                 <div className={styles.imageBlock}>
                     <div className={styles.smallImageBlock}>
                         <figure className={styles.figure}>
-                            <label className={styles.text}>
-                                Nome do Projeto
+                            <Image className={styles.image3}  src={imagem3} width={440} height={826} alt="imagem temporaria de testes"/>
+                            <label className={styles.text3}>
+                                {nomeprojeto3}
                             </label>
-                            <img className={styles.image} src="/img/RetanguloIlustrativo.png" width={440} height={826} alt="imagem temporaria de testes"/>
                         </figure>
                         <figure className={styles.figure}>
-                            <label className={styles.text}>
-                                Nome do Projeto
+                            <Image className={styles.image4}  src={imagem4} width={377} height={826} alt="imagem temporaria de testes"/>
+                            <label className={styles.text4}>
+                                {nomeprojeto4}
                             </label>
-                            <img className={styles.image} src="/img/RetanguloIlustrativo.png" width={377} height={826} alt="imagem temporaria de testes"/>
                         </figure>
                     </div>
                     <figure className={styles.figure}>
-                         <label className={styles.text}>
-                            Nome do Projeto
+                        <Image className={styles.image5}  src={imagem5} width={885} height={473} alt="imagem temporaria de testes"/>
+                         <label className={styles.text5}>
+                            {nomeprojeto5}
                         </label>
-                        <img className={styles.image} src="/img/RetanguloIlustrativo.png" width={885} height={473} alt="imagem temporaria de testes"/>
                     </figure>
                 </div>
             </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,23 @@
 import Issue7 from '../components/Issue7'
+import Img from "../../public/img/RetanguloIlustrativo.png"
 
 function Index() {
   return (
     <div>
-      <Issue7/>
+      <Issue7
+      titulo1 ="texto1"
+      titulo2 = "texto2"
+      imagem = {Img}
+      imagem2 = {Img}
+      imagem3 = {Img}
+      imagem4 = {Img}
+      imagem5 = {Img}
+      nomeprojeto1="projeto1"
+      nomeprojeto2="projeto2"
+      nomeprojeto3="projeto3"
+      nomeprojeto4="projeto4"
+      nomeprojeto5="projeto5"
+      />
     </div>
   );
 }
