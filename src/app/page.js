7f495119578335602954b88534fca7c435aace00
ArthurEly <arthur.ely78@gmail.com>
2023-10-14 00:05:22 -0300

    integração da 7 #28

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,8 @@ import '../styles/globals.css'
 import BannerFaisca from '@/components/BannerFaisca'
 import LearnMoreSection from '@/components/LearnMoreSection'
 import OurServices from '@/components/OurServices'
+import Cases from "@/components/Cases"
+import Img from "@public/img/RetanguloIlustrativo.png"
 
 export default function Home() {
   return (
@@ -10,6 +12,20 @@ export default function Home() {
       <BannerFaisca/>
       <LearnMoreSection />
       <OurServices />
+      <Cases
+        titulo1 ="texto1"
+        titulo2 = "texto2"
+        imagem = {Img}
+        imagem2 = {Img}
+        imagem3 = {Img}
+        imagem4 = {Img}
+        imagem5 = {Img}
+        nomeprojeto1="projeto1"
+        nomeprojeto2="projeto2"
+        nomeprojeto3="projeto3"
+        nomeprojeto4="projeto4"
+        nomeprojeto5="projeto5"
+      />
     </>
   )
 }
