95de543fc9621285111db8c2fef2cd742cabb6f7
ArthurEly <arthur.ely78@gmail.com>
2023-10-14 01:03:21 -0300

    integração da 14 #28

--- src/app/sobre-nos/page.js
+++ src/app/sobre-nos/page.js
@@ -2,6 +2,7 @@ import PurposeText from "@components/PurposeText"
 import AboutFaisca from "@components/AboutFaisca"
 import MembersArea from "@components/MembersArea"
 import Structure from "@components/Structure"
+import MEJ from "@components/MEJ"
 
 import styles from "./about.module.css"
 
@@ -12,6 +13,7 @@ function SobreNos(){
             <AboutFaisca />
             <MembersArea />
             <Structure />
+            <MEJ />
         </>
     )
 }
