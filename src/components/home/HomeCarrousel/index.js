baba15554527c1b7e3132305eb3c1aad5ea01cbd
ArthurTuy <arthursauer10@outlook.com>
2023-12-18 12:03:47 -0300

    #08
    08

--- src/components/home/HomeCarrousel/index.js
+++ src/components/home/HomeCarrousel/index.js
@@ -9,6 +9,7 @@ import "owl.carousel/dist/assets/owl.theme.default.css";
 import dynamic from "next/dynamic";
 
 
+<<<<<<< Updated upstream
 const OwlCarousel = dynamic(() => import('react-owl-carousel'), {
     ssr: false,
   });
@@ -28,3 +29,84 @@ export default function Slider() {
         </OwlCarousel>
     )
  }
+=======
+const Slider = (props) => {
+  const PartnersConfig = {
+    loop: true,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplaySpeed: 2000,
+    autoplayHoverPause: true,
+    responsiveClass: true,
+    mouseDrag: true,
+    touchDrag: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      700: {
+        items: 1,
+      },
+      1000: {
+        items: 1,
+      },
+    },
+    navContainerClass: `${styles.owlNav}`,
+    navClass: [`${styles.owlPrev}`, `${styles.owlNext}`],
+    navText: ['', ''],
+  };
+
+    return (
+      <div className={styles.depoimentos}>
+        <h2 className={styles.depoimentosTitle}>O QUE NOSSOS CLIENTES TEM A DIZER</h2>
+        <OwlCarousel   className={styles.slider}
+            {...PartnersConfig}   >
+          <div className={styles.item}>
+            <h2 className={styles.depoimentosText}>
+              “Trabalhar com a faísca foi uma excelente experiência, o resultado superou
+              toda e qualquer expectativa. Nos impactou de tal forma que buscamos estabelecer
+              uma parceria e recomendamos o trabalho sempre que possível”
+              <br></br><b>Equipe IECBiot</b>
+            </h2>
+          </div>
+          <div className={styles.item}>
+            <h2 className={styles.depoimentosText}>
+              “Trabalhar com a faísca foi uma excelente experiência, o resultado superou
+              toda e qualquer expectativa. Nos impactou de tal forma que buscamos estabelecer
+              uma parceria e recomendamos o trabalho sempre que possível”
+              <br></br><b>Equipe IECBiot</b>
+            </h2>
+          </div>
+          <div className={styles.item}>
+            <h2 className={styles.depoimentosText}>
+              “Trabalhar com a faísca foi uma excelente experiência, o resultado superou
+              toda e qualquer expectativa. Nos impactou de tal forma que buscamos estabelecer
+              uma parceria e recomendamos o trabalho sempre que possível”
+              <br></br><b>Equipe IECBiot</b>
+            </h2>
+          </div>
+          <div className={styles.item}>
+            <h2 className={styles.depoimentosText}>
+              “Trabalhar com a faísca foi uma excelente experiência, o resultado superou
+              toda e qualquer expectativa. Nos impactou de tal forma que buscamos estabelecer
+              uma parceria e recomendamos o trabalho sempre que possível”
+              <br></br><b>Equipe IECBiot</b>
+            </h2>
+          </div>
+          <div className={styles.item}>
+            <h2 className={styles.depoimentosText}>
+              “Trabalhar com a faísca foi uma excelente experiência, o resultado superou
+              toda e qualquer expectativa. Nos impactou de tal forma que buscamos estabelecer
+              uma parceria e recomendamos o trabalho sempre que possível”
+              <br></br><b>Equipe IECBiot</b>
+            </h2>
+          </div>
+        </OwlCarousel>
+      </div>
+    );
+  };
+
+  export default Slider;
+>>>>>>> Stashed changes
---
+++ src/components/projeto/ProjectCarousel/index.js
@@ -0,0 +1,70 @@
+'use client';
+import React, { useState, useEffect } from "react";
+import dynamic from "next/dynamic";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
+import styles from './carousel.module.css'
+import Image from 'next/image';
+import proj1 from './../../../../public/img/projeto/projeto1.png'
+import proj2 from './../../../../public/img/projeto/projeto2.png'
+import proj3 from './../../../../public/img/projeto/projeto3.png'
+
+
+var $ = require("jquery");
+if (typeof window !== "undefined") {
+  window.$ = window.jQuery = require("jquery");
+}
+
+const OwlCarousel = dynamic(() => import("react-owl-carousel"), {
+  ssr: false,
+});
+
+const Slider = (props) => {
+  const PartnersConfig2 = {
+    loop: true,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplaySpeed: 2000,
+    autoplayHoverPause: true,
+    responsiveClass: true,
+    mouseDrag: true,
+    touchDrag: true,
+    responsive: {
+      0: {
+        items: 3,
+      },
+      700: {
+        items: 3,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+    navContainerClass: `${styles.owlNav}`,
+    navClass: [`${styles.owlPrev}`, `${styles.owlNext}`],
+    navText: ['', ''],
+  };
+
+    return (
+      <div className={styles.projetos}>
+        <h2 className={styles.projetosTitle}>confira outros projetos</h2>
+        <OwlCarousel   className={styles.slider}
+            {...PartnersConfig2}   >
+          <div className={styles.item}>
+            <Image className={styles.imgcarimbo} src={proj1} alt={'nome'} width={1000} height={585}/>
+          </div>
+          <div className={styles.item}>
+            <Image className={styles.imgcarimbo} src={proj2} alt={'nome'} width={1000} height={585}/>
+          </div>
+          <div className={styles.item}>
+            <Image className={styles.imgcarimbo} src={proj3} alt={'nome'} width={1000} height={585}/>
+          </div>
+        </OwlCarousel>
+      </div>
+    );
+  };
+
+  export default Slider;
+
