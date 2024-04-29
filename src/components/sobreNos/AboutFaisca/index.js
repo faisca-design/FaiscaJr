9b87caf0922f30eff3a5a640783850d95a83d711
Unfollow1 <109428575+Unfollow1@users.noreply.github.com>
2024-04-29 01:19:17 -0300

    #11 arrumando versao mobile
    arrumei a versao mobile e tambem coloquei a imagem certa da faisca

--- src/components/sobreNos/AboutFaisca/index.js
+++ src/components/sobreNos/AboutFaisca/index.js
@@ -1,30 +1,30 @@
 import styles from './about.module.css'
 import Image from 'next/image'
-import img from "@img/sobreNos/squareLogo.png"
+import img from "@img/sobreNos/Logo-Faisca.png"
 
-function AboutFaisca ()
-{
+function AboutFaisca() {
     return (
         <div className={styles.preenchimento}>
             <div className={styles.container}>
-                <Image className={styles.containerImagem} src={img} width={240} height={240} alt="logo faisca"/>
-            
-                <p className={styles.paragrafo}>
-                    A Faísca é uma empresa júnior de design da UFRGS 
-                    que se <span className={styles.fraseDestaque}>destaca</span> no mercado por sua combina éção de <span className={styles.fraseDestaque}>criatividade </span>
-                    e <span className={styles.fraseDestaque}>eficiência</span>. Formada por estudantes universitários altamente 
-                    capacitados em design, a Faísca possui como principal diferencial
-                    a capacidade de trabalhar em estreita <span className={styles.fraseDestaque}>colaboração</span> com o cliente, 
-                    desde o início do projeto, visando compreender suas necessidades
-                    e expectativas, a fim de criar <span className={styles.fraseDestaque}>soluções</span> <span className={styles.fraseDestaque}>personalizadas</span> e que 
-                    atendam aos seus objetivos.
-                </p>
-                <p className={styles.paragrafo}> 
-                <br />Em suma, se você está em busca de uma empresa de design que 
-                    ofereça soluções criativas e eficientes, a Faísca é a escolha
-                    ideal. Conte conosco para ajudá-lo a alcançar seus objetivos 
-                    e superar suas expectativas.
-                </p>
+                <Image className={styles.containerImagem} src={img} width={350} height={350} alt="logo faisca" />
+                <div className={styles.text}>
+                    <p className={styles.paragrafo}>
+                        A Faísca é uma empresa júnior de design da UFRGS
+                        que se <span className={styles.fraseDestaque}>destaca</span> no mercado por sua combina ação de <span className={styles.fraseDestaque}>criatividade </span>
+                        e <span className={styles.fraseDestaque}>eficiência</span>. Formada por estudantes universitários altamente
+                        capacitados em design, a Faísca possui como principal diferencial
+                        a capacidade de trabalhar em estreita <span className={styles.fraseDestaque}>colaboração</span> com o cliente,
+                        desde o início do projeto, visando compreender suas necessidades
+                        e expectativas, a fim de criar <span className={styles.fraseDestaque}>soluções</span> <span className={styles.fraseDestaque}>personalizadas</span> e que
+                        atendam aos seus objetivos.
+                    </p>
+                    <p className={styles.paragrafo}>
+                        <br />Em suma, se você está em busca de uma empresa de design que
+                        ofereça soluções criativas e eficientes, a Faísca é a escolha
+                        ideal. Conte conosco para ajudá-lo a alcançar seus objetivos
+                        e superar suas expectativas.
+                    </p>
+                </div>
 
             </div>
         </div>
