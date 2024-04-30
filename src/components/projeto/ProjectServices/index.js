9185048490e29e93463ec3849302c5a8ebcdc630
Unfollow1 <109428575+Unfollow1@users.noreply.github.com>
2024-04-30 16:40:58 -0300

    diminuindo distancia entre issues
    so toquei a width de 100vh pra 100%

--- src/components/projeto/ProjectServices/index.js
+++ src/components/projeto/ProjectServices/index.js
@@ -4,30 +4,29 @@ import capa from '@img/projeto/Capa.png'
 import cracha from '@img/projeto/cracha.png'
 import xicara from '@img/projeto/xicara.png'
 
-function Issue19 ({titulo1, titulo2, titulo3})
-{
-    return(
+function Issue19({ titulo1, titulo2, titulo3 }) {
+    return (
         <div>
             <div className={styles.containerText}>
                 <h1 className={styles.text}>
                     {titulo1}
                 </h1>
-                <hr className={styles.bar} width={199}/>
+                <hr className={styles.bar} width={199} />
                 <h1 className={styles.text}>
                     {titulo2}
                 </h1>
-                <hr className={styles.bar} width={199}/>
+                <hr className={styles.bar} width={199} />
                 <h1 className={styles.text}>
                     {titulo3}
                 </h1>
             </div>
             <div>
                 <div className={styles.upperImages}>
-                    <Image className={styles.smallImage1} src={capa} width={680} height={785} alt="cartão de visitas"/>
-                    <Image className={styles.smallImage2} src={cracha} width={680} height={785} alt="feed do instagram"/>
+                    <Image className={styles.smallImage1} src={capa} width={680} height={785} alt="cartão de visitas" />
+                    <Image className={styles.smallImage2} src={cracha} width={680} height={785} alt="feed do instagram" />
                 </div>
                 <div className={styles.lowerImages}>
-                    <Image className={styles.image} src={xicara} width={1468} height={906} alt="cartão de visitas"/>
+                    <Image className={styles.image} src={xicara} width={1468} height={906} alt="cartão de visitas" />
                 </div>
             </div>
         </div>
