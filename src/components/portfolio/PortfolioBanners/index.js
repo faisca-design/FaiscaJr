7e37cc4b44d660b979e78031f7341f4a908cfdf4
Carol <carolinapfal2004@gmail.com>
2024-07-07 22:42:50 -0300

    Portfólio: efeito zoom, letras desaparecem, sem linha preta entre imagens, px para vw

--- src/components/portfolio/PortfolioBanners/index.js
+++ src/components/portfolio/PortfolioBanners/index.js
@@ -7,54 +7,54 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 function BannersFaisca() {
-    return (
-      <div className={styles.topContainer}>
-        <div className={styles.imageContainer}>
-          <Link href="https://www.behance.net/gallery/106148143/Chamas-da-Insignificancia-Editorial?locale=pt_BR">
-            <div className={styles.imageOverlay}>
-              <Image src={chamasDaInsignificancia} alt="Chamas da Insignificância" className={styles.image} />
-            </div>
-          </Link>
-          <div className={styles.textContainer}>
-            <h1 className={styles.textTitle}>CHAMAS DA <br /> INSIGNIFICÂNCIA</h1>
-            <p className={styles.textDescription}>DIAGRAMAÇÃO <br /> EDITORIAL</p>
+  return (
+    <div className={styles.topContainer}>
+      <div className={styles.imageContainer}>
+        <Link href="https://www.behance.net/gallery/106148143/Chamas-da-Insignificancia-Editorial?locale=pt_BR">
+          <div className={styles.imageOverlay}>
+            <Image src={chamasDaInsignificancia} alt="Chamas da Insignificância" className={styles.image} />
           </div>
-        </div>
-        <div className={styles.imageContainer}>
-          <Link href="https://www.instagram.com/spotlight_connection/">
-            <div className={styles.imageOverlay}>
-              <Image src={spotlight} alt="Spotlight" className={styles.image} />
-            </div>
-          </Link>
-          <div className={styles.textContainer}>
-            <h1 className={styles.textTitle}>SPOT <br /> LIGHT</h1>
-            <p className={styles.textDescription}>IDENTIDADE VISUAL <br /> APLICAÇÕES</p>
-          </div>
-        </div>
-        <div className={styles.imageContainer}>
-          <Link href="https://www.behance.net/gallery/140464381/Zakto?tracking_source=search_projects|zakto&l=0">
-            <div className={styles.imageOverlay}>
-              <Image src={zakto} alt="Zakto" className={styles.image} />
-            </div>
-          </Link>
-          <div className={styles.textContainer}>
-            <h1 className={styles.textTitle}>ZAKTO</h1>
-            <p className={styles.textDescription}>DIAGRAMAÇÃO <br /> EDITORIAL</p>
-          </div>
-        </div>
-        <div className={styles.imageContainer}>
-          <Link href="https://www.behance.net/gallery/146840537/Bako-Bebidas?locale=pt_BR">
-            <div className={styles.imageOverlay}>
-              <Image src={bakoBebidas} alt="Bako Bebidas" className={styles.image} />
-            </div>
-          </Link>
-          <div className={styles.textContainer}>
-            <h1 className={styles.textTitle}>BAKO <br /> BEBIDAS</h1>
-            <p className={styles.textDescription}>IDENTIDADE VISUAL <br /> APLICAÇÕES</p>
-          </div>
+        </Link>
+        <div className={styles.textContainer}>
+          <h1 className={styles.textTitle}>CHAMAS DA <br /> INSIGNIFICÂNCIA</h1>
+          <p className={styles.textDescription}>DIAGRAMAÇÃO <br /> EDITORIAL</p>
         </div>
       </div>
-    );
-  }
+      <div className={styles.imageContainer}>
+        <Link href="https://www.instagram.com/spotlight_connection/">
+          <div className={styles.imageOverlay}>
+            <Image src={spotlight} alt="Spotlight" className={styles.image} />
+          </div>
+        </Link>
+        <div className={styles.textContainer}>
+          <h1 className={styles.textTitle}>SPOT <br /> LIGHT</h1>
+          <p className={styles.textDescription}>IDENTIDADE VISUAL <br /> APLICAÇÕES</p>
+        </div>
+      </div>
+      <div className={styles.imageContainer}>
+        <Link href="https://www.behance.net/gallery/140464381/Zakto?tracking_source=search_projects|zakto&l=0">
+          <div className={styles.imageOverlay}>
+            <Image src={zakto} alt="Zakto" className={styles.image} />
+          </div>
+        </Link>
+        <div className={styles.textContainer}>
+          <h1 className={styles.textTitle}>ZAKTO</h1>
+          <p className={styles.textDescription}>DIAGRAMAÇÃO <br /> EDITORIAL</p>
+        </div>
+      </div>
+      <div className={styles.imageContainer}>
+        <Link href="https://www.behance.net/gallery/146840537/Bako-Bebidas?locale=pt_BR">
+          <div className={styles.imageOverlay}>
+            <Image src={bakoBebidas} alt="Bako Bebidas" className={styles.image} />
+          </div>
+        </Link>
+        <div className={styles.textContainer}>
+          <h1 className={styles.textTitle}>BAKO <br /> BEBIDAS</h1>
+          <p className={styles.textDescription}>IDENTIDADE VISUAL <br /> APLICAÇÕES</p>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default BannersFaisca;
