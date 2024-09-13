9647e7ef26772863fe54443f275b341c0d3e7a25
ArthurTuy <arthursauer10@outlook.com>
2024-09-12 23:15:15 -0300

    props depoimentos

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,44 @@
-import '../styles/globals.css'
-
-import BannerFaisca from '@/components/home/BannerFaisca'
-import LearnMoreSection from '@/components/home/LearnMoreSection'
-import OurServices from '@/components/home/OurServices'
-import Cases from "@/components/home/Cases"
-import HomeCarousel from '@/components/home/HomeCarrousel'
+import BannerFaisca from '@/components/home/BannerFaisca';
+import LearnMoreSection from '@/components/home/LearnMoreSection';
+import OurServices from '@/components/home/OurServices';
+import Cases from "@/components/home/Cases";
+import HomeCarousel from '@/components/home/HomeCarrousel';
 
 export default function Home() {
+  const depoimentos = [
+    {
+      texto: "Além do excelente atendimento, cumprimento dos prazos e o resultado que superou minha expectativa, gostaria de pontuar que o mais impactante para mim foi que na apresentação dos primeiros materiais eu percebi o quão bem o pessoal captou a minha personalidade conseguindo transpor tudo o que eu tinha em mente através da identidade visual.",
+      cliente: "Adriana Ilha",
+    },
+    {
+      texto: "Trabalhar com a Faísca foi uma excelente experiência, o resultado superou toda e qualquer expectativa. Nos impactou de tal forma que buscamos estabelecer uma parceria e recomendamos o trabalho sempre que possível.",
+      cliente: "Equipe IECBiot",
+    },
+    {
+      texto: "O envolvimento e dedicação da equipe da Faísca fez toda diferença. Desde a primeira reunião fiquei impactado pelas contribuições. Recebi mais do que esperava. Foi muito bom.",
+      cliente: "Edson Lima",
+    },
+    {
+      texto: "A equipe abraçou o projeto como ninguém até hoje abraçou, sugerindo ideias e trabalhando no material que já existia. Posso tranquilamente dizer que foi a melhor equipe que já trabalhei! Um beijo no coração deles!",
+      cliente: "Pedro Valente",
+    },
+    {
+      texto: "Acredito que a nova identidade visual da MOVA com certeza transmite agora o que realmente queríamos que transmitisse! Vocês conseguiram captar tudo o que queríamos e transformaram em algo muito legal, que realmente representa a MOVA! Sempre fomos muito acolhidos ao longo do projeto, foi uma experiência completamente positiva.",
+      cliente: "Camila Moreira, MOVA",
+    },
+
+
+  ];
+
   return (
     <>
       <BannerFaisca />
       <LearnMoreSection />
       <OurServices />
-      <Cases
-        leftTittle="CASES"
-        rightTittle="SELECIONADOS"
-      />
-      <HomeCarousel />
+      <Cases 
+      leftTittle="CASES" 
+      rightTittle="SELECIONADOS" />
+      <HomeCarousel depoimentos={depoimentos} />
     </>
-  )
+  );
 }
