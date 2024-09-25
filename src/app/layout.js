5cbe2d8dc358a70e5fe005bb34ea68b3d706a38f
Unfollow1 <109428575+Unfollow1@users.noreply.github.com>
2024-09-25 01:19:14 -0300

    degradê fundo finalizado #2
    fazendo o fundo em javaScript ao invés de css

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import DynamicBackground from "@components/dynamicBackground";
 import Footer from "@/components/Layout/Footer";
 import Navbar from "@/components/Layout/Navbar";
 import styles from "@styles/pages.module.css"
@@ -9,13 +10,16 @@ export default function Layout({ children }) {
             <head>
                 <title>Faísca</title>
             </head>
-            <body style={{margin:0}}>
+            <body style={{margin: 0, padding: 0, backgroundColor: '#1E181A', minHeight: '100vh'}}>      {/* propriedades adicionadas para o bakcground funcionar :(*/}
+                <div style={{position: 'relative', minHeight: '100vh'}}>
+                    <DynamicBackground />
                     <Navbar/>
-                        <div className={styles.page}>
-                            {children}
-                        </div>
+                    <div className={styles.page}>
+                        {children}
+                    </div>
                     <Footer/>
+                </div>
             </body>
         </html>
     )
-  }
+}
