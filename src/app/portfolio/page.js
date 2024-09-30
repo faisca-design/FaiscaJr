9b1fc9389393694f6b43d260708a2d0380c4b6b3
Unfollow1 <109428575+Unfollow1@users.noreply.github.com>
2024-09-29 22:52:43 -0300

    limpando projeto
    apagando coisas que não serão usadas

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -1,6 +1,5 @@
 import PortfolioBanners from "@components/portfolio/PortfolioBanners"
 import PortfolioCarousel from "@components/portfolio/PortfolioCarousel"
-import ProManegers from "@components/projeto/ProjectManagers"
 
 function Portfolio(){
   return (
