e19a69e2a8c5aad0c1e644453edc672efef03acb
Unfollow1 <109428575+Unfollow1@users.noreply.github.com>
2024-10-14 02:11:07 -0300

    dependencias painel netlify

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import DynamicBackground from "@components/dynamicBackground";
 import Footer from "@/components/Layout/Footer";
 import Navbar from "@/components/Layout/Navbar";
 import styles from "@styles/pages.module.css"
+import Script from "next/script";
 import "@styles/globals.css"
 
 export default function Layout({ children }) {
@@ -9,6 +10,10 @@ export default function Layout({ children }) {
         <html lang="pt-br">
             <head>
                 <title>Faísca</title>
+                <Script
+                    async
+                    src="https://identity.netlify.com/v1/netlify-identity-widget.js"
+                />
             </head>
             <body style={{ margin: 0, padding: 0, backgroundColor: '#1E181A', minHeight: '100vh' }}>      {/* propriedades adicionadas para o bakcground funcionar :(*/}
                 <div style={{ position: 'relative', minHeight: '100vh' }}>
