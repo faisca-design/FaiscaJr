5b9962f7b5809af111c9e078a2d7251ddec01203
gleydson campos <gleydsonsousa33@gmail.com>
2024-12-28 23:22:11 -0300

    CMS estatico sobre nós feito
    100% funcional e setado com o que ta no desing do figma, agora so eles alterarem como quiserem.

--- src/components/sobreNos/MEJ/index.js
+++ src/components/sobreNos/MEJ/index.js
@@ -6,16 +6,16 @@ import StyledText from "@/components/textoEstilizado/textoEstilizado"
 function MEJ({ tittle, phrase, mainImg, brasilJrImg, NejImg, fejersImg, designUFRGSImg}) {
     return (
         <div className={styles.container}>
-            <Image className={styles.imge} src={mainImg} alt={"error"} width={0} height={420} />
+            <Image className={styles.imge} src={mainImg} alt={"error"} width={3000} height={3000} />
             <div className={styles.bar}></div>
             <div className={styles.rightContent}>
                 <h1><StyledText text={tittle}/></h1>
                 <p><StyledText text={phrase}/></p>
                 <div className={styles.apoiadores}>
-                    <Image className={styles.logos} src={brasilJrImg} width={0} height={100} alt="brasil" />
-                    <Image className={styles.nejpoa} src={NejImg} width={0} height={100} alt="NejPoa" />
-                    <Image className={styles.fejers} src={fejersImg} width={0} height={100} alt="Fejers" />
-                    <Image className={styles.logos} src={designUFRGSImg} width={0} height={100} alt="design ufrgs" />
+                    <Image className={styles.logos} src={brasilJrImg} width={1000} height={1000} alt="brasil" />
+                    <Image className={styles.nejpoa} src={NejImg} width={1000} height={1000} alt="NejPoa" />
+                    <Image className={styles.fejers} src={fejersImg} width={1000} height={1000} alt="Fejers" />
+                    <Image className={styles.logos} src={designUFRGSImg} width={1000} height={1000} alt="design ufrgs" />
                 </div>
             </div>
         </div>
--- src/components/sobreNos/Structure/CardRightText/index.js
+++ src/components/sobreNos/Structure/CardRightText/index.js
@@ -5,7 +5,7 @@ import StyledText from "@/components/textoEstilizado/textoEstilizado"
 function CardRightText({ imgPath, erro, tittle, text }) {
     return (
         <div className={styles.container}>
-            <Image src={imgPath} alt={erro} className={styles.img} />
+            <Image src={imgPath} alt={erro} className={styles.img} width={3000} height={3000}/>
             <div className={styles.textContainer}>
                 <h1> <StyledText text={tittle} /></h1>
                 <p><StyledText text={text} /></p>
