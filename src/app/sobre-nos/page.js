bc4e812b5206d2deda4f5c40f83ff9d27ec52745
gleydson campos <gleydsonsousa33@gmail.com>
2024-12-29 22:20:48 -0300

    CMS sobre nós completo #29
    todo CMS do sobre nós funcional e pronto para uso, além de setado como eles pediram, basta fazer alterações desejadas pelo painel.

--- src/app/sobre-nos/page.js
+++ src/app/sobre-nos/page.js
@@ -4,22 +4,26 @@ import AboutFaisca from "@components/sobreNos/AboutFaisca"
 import MembersArea from "@components/sobreNos/MembersArea"
 import Structure from "@/components/sobreNos/Structure"
 import MEJ from "@components/sobreNos/MEJ"
-import { handleJSONfile } from '../../utils/functions/jsonHandler.js'
+import { handleJSONfile, handleJSONfiles } from '../../utils/functions/jsonHandler.js'
 import Members from "@components/sobreNos/Members"
 
 async function getData() {
     const sobreNosData = await handleJSONfile('content/sobreNos/sobreNosPage.json');
-    
+    const membersData = await handleJSONfiles('content/membros');
+
     if (!sobreNosData) {
         throw new Error('Falha ao carregar dados da página sobre nós');
     }
 
-    return sobreNosData;
+    return {
+        sobreNosData,
+        membersData
+    };
 }
 
 async function SobreNos() {
-    const data = await getData();
-    const { purpose, aboutFaisca, membersArea, structure, mej } = data;
+    const { sobreNosData, membersData } = await getData();
+    const { purpose, aboutFaisca, membersArea, structure, mej } = sobreNosData;
 
     return (
         <div className={styles.withPadding}>
@@ -31,20 +35,16 @@ async function SobreNos() {
                 logo={aboutFaisca.logo}
             />
 
-           {/*<MembersArea
+            <Members
                 tittle={membersArea.tittle}
                 subTittle={membersArea.subTittle}
-            /> */} 
-
-            <Members                 
-                tittle={membersArea.tittle}
-                subTittle={membersArea.subTittle}
+                membersData={membersData}
             />
 
             <Structure
                 tittle={structure.tittle}
                 phrase={structure.phrase}
-                
+
                 imgOne={structure.sectionOne.img}
                 subTittleOne={structure.sectionOne.subTittle}
                 textOne={structure.sectionOne.text}
--- src/components/sobreNos/Members/index.js
+++ src/components/sobreNos/Members/index.js
@@ -1,29 +1,32 @@
+// allMembers.js
 import styles from './allMembers.module.css'
 import Card from './card';
-import membersData from './../../../data/testMembros.json'
 import StyledText from "@/components/textoEstilizado/textoEstilizado"
 
-function allCards({ tittle, subTittle }) {
-    return (
-        <div>
-            <h2 className={styles.title}><StyledText text={tittle}/></h2>
-            <h1 className={styles.subTittle}><StyledText text={subTittle}/></h1>
-            <section className={styles.container}>
-                <div className={styles.row}>
-                    {membersData.map((DMembers) => (
-                        <Card
-                            key={DMembers.id}
-                            members={DMembers.members || [{
-                                nome: DMembers.nome,
-                                ciclo: DMembers.ciclo,
-                                imagem: DMembers.imagem
-                            }]}
-                        />
-                    ))}
-                </div>
-            </section>
+function AllCards({ tittle, subTittle, membersData }) {
+  // Ordena os membros pela ordem definida
+  const sortedMembers = membersData.sort((a, b) => a.ordem - b.ordem);
+
+  return (
+    <div>
+      <h2 className={styles.title}><StyledText text={tittle}/></h2>
+      <h1 className={styles.subTittle}><StyledText text={subTittle}/></h1>
+      <section className={styles.container}>
+        <div className={styles.row}>
+          {sortedMembers.map((DMembers) => (
+            <Card
+              key={DMembers.id}
+              members={DMembers.members || [{
+                nome: DMembers.nome,
+                ciclo: DMembers.ciclo,
+                imagem: DMembers.imagem
+              }]}
+            />
+          ))}
         </div>
-    )
+      </section>
+    </div>
+  )
 }
 
-export default allCards;
+export default AllCards;
