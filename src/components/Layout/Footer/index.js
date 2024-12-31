2bc4c8c036d59178ec5a20c00c015245c5ed36bb
gleydson campos <gleydsonsousa33@gmail.com>
2024-12-31 14:42:48 -0300

    cms footer e nav completo

--- src/components/Layout/Footer/index.js
+++ src/components/Layout/Footer/index.js
@@ -1,22 +1,26 @@
 import styles from './Footer.module.css';
-import pageStyles from '../../../styles/pages.module.css'; 
+import pageStyles from '../../../styles/pages.module.css';
 import Item from "./item"
 
 function Footer({ socialMedia }) {
-    return (
-        <footer className={`${styles.footer} ${pageStyles.footer}`}>
-            <ul className={styles.social_list}>
-                {socialMedia.map((item, index) => (
-                    <Item 
-                        key={index}
-                        icon={item.icon}
-                        link={item.link}
-                        altText={item.altText}
-                    />
-                ))}
-            </ul>
-        </footer>
-    );
-}
 
-export default Footer;
+        // Ordena as redes sociais com base no campo order
+        const sortedSocialMedia = [...socialMedia].sort((a, b) => a.order - b.order);
+
+        return (
+            <footer className={`${styles.footer} ${pageStyles.footer}`}>
+                <ul className={styles.social_list}>
+                    {sortedSocialMedia.map((item, index) => (
+                        <Item
+                            key={index}
+                            icon={item.icon}
+                            link={item.link}
+                            altText={item.altText}
+                        />
+                    ))}
+                </ul>
+            </footer>
+        );
+    }
+
+    export default Footer;
