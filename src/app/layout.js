071176c8eaa8c51757772b7b06236205b6d68dd6
gleydson campos <gleydsonsousa33@gmail.com>
2025-01-12 15:54:58 -0300

    bugfix 3

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import BackgroundManager from "@/components/backgroundManager"
+import BackgroundManager from "../components/backgroundManager"
 import Footer from "@/components/Layout/Footer";
 import Navbar from "@/components/Layout/Navbar";
 import styles from "@styles/pages.module.css"
