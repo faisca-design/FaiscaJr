12a3e91b1a072af25c455a205ee123508d35eb89
gleydson campos <gleydsonsousa33@gmail.com>
2025-01-23 23:31:35 -0300

    portfolio
    readicionando mudanças antigas

--- src/components/portfolio/PortfolioBanners/Project/index.js
+++ src/components/portfolio/PortfolioBanners/Project/index.js
@@ -5,17 +5,18 @@ import StyledText from "@/components/textoEstilizado/textoEstilizado"
 
 function Project({ path, image, erro, titulo, desc }) {
   return (
-    <div className={styles.imageContainer}>
-      <Link href={path}>
-        <div className={styles.imageOverlay}>
+    <Link href={path} className={styles.projectLink}>
+      <div className={styles.imageContainer}>
+        <div className={styles.imageWrapper}>
           <Image src={image} alt={erro} className={styles.image} width={3000} height={3000} />
         </div>
-      </Link>
-      <div className={styles.textContainer}>
-        <h1 className={styles.textTitle}><StyledText text={titulo}/></h1>
-        <p className={styles.textDescription}><StyledText text={desc}/></p>
+        <div className={styles.imageOverlay}></div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.textTitle}><StyledText text={titulo}/></h1>
+          <p className={styles.textDescription}><StyledText text={desc}/></p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
