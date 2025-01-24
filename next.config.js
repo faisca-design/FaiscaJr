735a9cd8073330d53c506dd056dd068d3c7231a6
gleydson campos <gleydsonsousa33@gmail.com>
2025-01-24 05:28:16 -0300

    incluindo json's no build
    adicionando outputFileTracingIncludes para incluir os arquivos JSON no build.

--- next.config.js
+++ next.config.js
@@ -1,20 +1,22 @@
 /** @type {import('next').NextConfig} */
-
 const webpack = require("webpack");
 
 const nextConfig = {
   experimental: {
-    appDir: true,
+    outputFileTracingIncludes: {
+      '/api/**/*': ['content/**/*']
+    }
   },
   reactStrictMode: true,
   swcMinify: true,
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.plugins.push(
-     new webpack.ProvidePlugin({
-     $: "jquery",
-     jQuery: "jquery",
-     "window.jQuery": "jquery",
-    }));
+      new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        "window.jQuery": "jquery",
+      })
+    );
     return config;
   }
 }
