096e00a542b1c163ad1a83955808ec937a84b782
gleydson campos <gleydsonsousa33@gmail.com>
2025-01-27 03:09:23 -0300

    teste

--- src/components/RefreshHandler/index.js
+++ src/components/RefreshHandler/index.js
@@ -1,41 +1,38 @@
 'use client';
-
+// components/RefreshHandler.js
 import { useEffect } from 'react';
-import { usePathname } from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation';
 
 export default function RefreshHandler() {
+  const router = useRouter();
   const pathname = usePathname();
 
   const normalizeUrl = (url) => {
-    // Divide a URL em partes
     const parts = url.split('/');
-    // Pega o último segmento (nome do projeto)
     const projectName = parts[parts.length - 1];
-    // Normaliza apenas o nome do projeto
     const normalizedName = projectName.toLowerCase();
-    // Reconstrói a URL substituindo apenas o nome do projeto
     parts[parts.length - 1] = normalizedName;
     return parts.join('/');
-  };
-
-  const handleRefresh = () => {
-    // Normaliza a URL antes do refresh
-    const normalizedPath = normalizeUrl(pathname);
-    const timestamp = Date.now().toString().slice(-4);
-    window.location.href = normalizedPath + `?v=${timestamp}`;
   };
 
   useEffect(() => {
     const handleKeyDown = (e) => {
       if (e.key === 'F5' || (e.ctrlKey && e.key === 'r')) {
         e.preventDefault();
-        handleRefresh();
+        
+        // Apenas normaliza a URL sem refresh
+        const normalizedPath = normalizeUrl(pathname);
+        
+        // Usa o router.replace para não adicionar nova entrada no histórico
+        if (pathname !== normalizedPath) {
+          router.replace(normalizedPath);
+        }
       }
     };
 
     window.addEventListener('keydown', handleKeyDown);
     return () => window.removeEventListener('keydown', handleKeyDown);
-  }, [pathname]);
+  }, [router, pathname]);
 
   return null;
 }
